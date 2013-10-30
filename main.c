1086de583d1e9d1bb9b67dd6c612bd80b313a0d9
luxiaotong <luxiaotong713@sina.com>
2013-10-30 16:56:55 +0800

    init
    init

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: @两荤两素三两饭
+ *
+ * Created on 2013年10月30日, 下午4:54
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+	return (EXIT_SUCCESS);
+}
+
